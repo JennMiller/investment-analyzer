949be43f7a19b8e6a1ae387d14aec9bb6e3f746b
Enosh Anwar <enosh.anwar12@gmail.com>
2020-04-14 20:08:36 -0500

    Update interest formulae

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import PercentageInterest from './components/PercentageInterest';
+import React from "react";
+import PercentageInterest from "./components/PercentageInterest";
+import styled from "styled-components";
+import { Heading, ThemeProvider } from "pcln-design-system";
+
+const Container = styled(ThemeProvider)`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  flex-direction: column;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Simple Interest Calculator</h1>
+    <Container>
+      <Heading.h1>Simple Interest Calculator</Heading.h1>
       <PercentageInterest />
-    </div>
+    </Container>
   );
 }
 
--- src/components/PercentageInterest/PercentageInterest.js
+++ src/components/PercentageInterest/PercentageInterest.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { themeGet } from "@styled-system/theme-get";
+import { Select, Label, Input } from "pcln-design-system";
 
 const Container = styled.div`
   display: flex;
+  justify-content: center;
   flex-direction: column;
   font-size: 30px;
-`;
 
-const StyledLabel = styled.label`
-  margin-bottom: 10px;
-  input {
-    margin-left: 10px;
-    height: 32px;
-    font-size: 25px;
-    width: 120px;
+  label {
+    margin: 10px 0 5px;
+    font-size: ${themeGet("fontSizes.2")}px;
   }
 `;
+
 const StyledInterest = styled.div`
   color: ${({ interest }) =>
     interest === 0 ? "black" : interest > 0 ? "green" : "red"};
+`;
+
+const StyledSelect = styled(Select)`
+  /* width: 100px; */
 `;
 
 function PercentageInterest() {
@@ -28,28 +31,41 @@ function PercentageInterest() {
 
   useEffect(() => {
     if (!buy || !sell) setInterest(0);
-    else setInterest(((1 - buy / sell) * 100).toFixed(2));
+    else setInterest((((sell - buy) / buy) * 100).toFixed(2));
   }, [buy, sell]);
+
+  const updateState = (setState, event) => {
+    setState(event.target.value);
+  };
 
   return (
     <Container>
-      <StyledLabel htmlFor="buy">
-        Buy at:
-        <input
-          name="buy"
-          type="number"
-          value={buy || ''}
-          onChange={(event) => setBuy(event.target.value)}
-        />
-      </StyledLabel>
-      <StyledLabel htmlFor="sell">
-        Sell at:
-        <input
-          type="number"
-          value={sell  || ''}
-          onChange={(event) => setSell(event.target.value)}
-        />
-      </StyledLabel>
+      <Label htmlFor="currency">Currency</Label>
+      <StyledSelect name="currency">
+        <option value="USD">USD</option>
+        <option value="CAD">CAD</option>
+      </StyledSelect>
+
+      <Label htmlFor="buy" autoHide>
+        Buy at
+      </Label>
+      <Input
+        id="buy"
+        name="buy"
+        type="number"
+        value={buy || ""}
+        onChange={(event) => updateState(setBuy, event)}
+      />
+
+      <Label autoHide>Sell at</Label>
+      <Input
+        id="sell"
+        name="sell"
+        type="number"
+        value={sell || ""}
+        onChange={(event) => updateState(setSell, event)}
+      />
+
       <div>
         Interest:
         <StyledInterest interest={interest}>{interest}%</StyledInterest>
