d16e468c8ca2581a1d63a0382ff8bfe6d4c30472
Enosh Anwar <enosh.anwar12@gmail.com>
2020-04-16 23:51:48 -0500

    Make the app themeable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { ThemeProvider, getPaletteColor } from 'pcln-design-system';
+import { ThemeProvider, getPaletteColor, Button } from 'pcln-design-system';
 import Home from './components/Home';
 
 const Container = styled(ThemeProvider)`
@@ -12,14 +12,48 @@ const Container = styled(ThemeProvider)`
 const MainContent = styled.main`
   display: flex;
   align-items: center;
-  padding: 90px 10% 0;
+  padding: 40px 10% 0;
   color: ${getPaletteColor('text.base')};
   background: ${getPaletteColor('background.lightest')};
 `;
 
+const ThemeSwitcher = styled(Button)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100px;
+`;
+
+const themePalettes = {
+  dark: {
+    palette: {
+      text: {
+        lightest: '#fff',
+        light: '#fff',
+        base: '#fff'
+      },
+      background: {
+        lightest: '#111'
+      },
+      border: {
+        light: '#fff'
+      }
+    }
+  }
+};
+
 function App() {
+  const [theme, setTheme] = useState('light');
+  const isDark = theme === 'dark';
+  const switchTheme = () => {
+    setTheme(isDark ? 'light' : 'dark');
+  };
+
   return (
-    <Container>
+    <Container theme={themePalettes[theme]}>
+      <ThemeSwitcher onClick={switchTheme}>
+        {isDark ? 'LIGHT' : 'DARK'}
+      </ThemeSwitcher>
       <MainContent>
         <Home />
       </MainContent>
