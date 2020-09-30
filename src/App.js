f232d343524b2b5d6fa9dcd9f7af167467643b55
Enosh Anwar <enosh.anwar12@gmail.com>
2020-09-30 16:24:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { ThemeProvider, getPaletteColor, Button } from 'pcln-design-system';
 import Home from './components/Home';
